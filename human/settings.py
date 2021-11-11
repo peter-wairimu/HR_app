49c81c47f54efa54786d29ef0d201620db4c044d
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-11-11 20:56:32 +0300

    added env file

--- human/settings.py
+++ human/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#v6uijggvqm9y)_$$ps+0)r%wng7uk&f0h(%y_6u%3h-6n=^t*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
