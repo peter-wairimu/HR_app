461734aeac936dd322d95cce6838858dee00b919
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-11-11 20:50:13 +0300

    modiefied the page

--- humanpro/models.py
+++ humanpro/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 
 
@@ -25,7 +24,7 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=50)
-    manager = models.ForeignKey('self',null=True,related_name='employees',on_delete=models.SET_NULL)
-
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    employee_type = models.CharField(max_length=3, choices=EMPLOYEE_TYPES, default=STANDARD)
     def __str__(self):
         return self.first_name + ' ' + self.last_name
--- humanpro/views.py
+++ humanpro/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Employee
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the humanpro index.")
+    employees = Employee.objects.order_by('id').all()
+    return render(request, 'human/index.html', {'employees': employees})
