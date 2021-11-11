cd755e176f552189bffae4b73b01e57f5bafc182
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-11-11 19:29:18 +0300

    updated settings.py

--- human/settings.py
+++ human/settings.py
@@ -73,11 +73,12 @@ WSGI_APPLICATION = 'human.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'login',
+        'USER': 'moringa',
+    'PASSWORD':'wairimu22',
     }
 }
 
